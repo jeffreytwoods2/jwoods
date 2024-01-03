f331fd2cfe1d8ae351b7f7dc2b802e24c9817307
jeffreywoods <jeffreytwoods2@icloud.com>
2024-01-02 17:43:53 -0700

    Webmanifest fixes

--- src/routes/(dashboard)/dash/+layout.svelte
+++ src/routes/(dashboard)/dash/+layout.svelte
@@ -9,6 +9,7 @@
 	<link rel="icon" href={favicon} type="image/x-icon" />
 	<meta property="og:type" content="website" />
 	<meta property="og:site_name" content="jwoods.dev" />
+    <meta name="theme-color" content="#4A6F28" />
 	<link rel="apple-touch-icon" href={appleIcon} />
     <link rel="manifest" href="/manifest.webmanifest" type="application/manifest+json">
 </svelte:head>
